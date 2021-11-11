cdbac5006b156aacf7d580047f5d9682cd28a9fe
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-11-11 13:44:34 +0100

    skapade en struktur , och la in porppertys i Account.cs

---
+++ BankSystem2/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem2
+{
+    public abstract class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public int Salary { get; set; }
+
+        public void ShowSalary()
+        {
+            Console.WriteLine($"{Salary}");
+        }
+        public void ShowRole()
+        {
+            Console.WriteLine($"{Role}");
+        }
+    }
+}
