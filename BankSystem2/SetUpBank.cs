7543f39fe4825f2f04c61e97d587fafd09ab0043
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-11-11 14:21:36 +0100

    Gjorde funkunalitet för att logga in en användare

--- BankSystem2/SetUpBank.cs
+++ BankSystem2/SetUpBank.cs
@@ -10,5 +10,34 @@ namespace BankSystem2
     {
         public static List<Account> listOfUsers = new List<Account> { new Admin() };
 
+        public static void LogIn()
+        {
+            bool logInSuccesful = false;
+            do
+            {
+                Console.WriteLine("\n Login");
+                Console.Write("Enter Username : ");
+                var username = Console.ReadLine();
+                Console.WriteLine("Enter Password : ");
+                var password = Console.ReadLine();
+
+                var user = listOfUsers.FirstOrDefault(u => u.Username == username && u.Password == password);
+                if (user != null)
+                {
+                    HandleUser(user);
+                }
+            } while (!logInSuccesful);
+        }
+
+        private static void HandleUser(Account logedInAccount)
+        {
+            if (logedInAccount is Admin)
+            {
+                var admin = logedInAccount as Admin;
+               
+            }
+
+
+        }
     }
 }
